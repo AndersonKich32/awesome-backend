ecef1b41d2a8ae8164e0d2b26cc2d8edff2baf37
Your Name <you@example.com>
2021-01-20 11:31:16 -0300

    dep

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const knex = require('knex')
 
 const configurations = require('../../knexfile')
-
+knex.migrate.latest([configurations])
 const connection = knex(configurations.development);
 
 module.exports = connection;
