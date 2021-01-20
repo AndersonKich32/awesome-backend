ed4a72202291630d33268ce41a4ff2a99e6587bc
Your Name <you@example.com>
2021-01-20 11:12:10 -0300

    uuuuuu

--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,8 @@ module.exports = {
     connection: {
         host : 'mysql742.umbler.com',  
         user : 'kich',      
-        password : '97408469Bdc',  
-        database : 'lojaKich'  
+        password : '97408469Kich',  
+        database : 'lojakich'  
     },
     migrations:{
       directory: './src/database/migrations'
